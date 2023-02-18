4626772d7f6f1160dc798ae4d1c2630b3c588f7f
manuele sapia <manulon12@outlook.com>
2023-02-18 07:56:35 +0000

    more chnages

--- Centrica.Web.Html/src/js/main.js
+++ Centrica.Web.Html/src/js/main.js
@@ -19,24 +19,73 @@ function init() {
 }
 var navigationMobile = {
   init: function () {
-    const mobileButton = document.querySelector("button.mobile__menu");
-    mobileButton.addEventListener("click", function () {
-      mobileButton.classList.toggle("is-open");
-      document.querySelector(".main__menu").classList.toggle("is__active");
-      // if (mobileButton.className.includes("is-open")) {
-      //   document
-      //     .querySelector(".sub__menu__main")
-      //     .classList.remove("is__active");
-      //   document
-      //     .querySelector(".main__menu__item__holder")
-      //     .classList.remove("is__active");
-      // }
+    const pageHeader = document.querySelector(".main__navigation");
+    const toggleMenu = pageHeader.querySelector(".toggle-menu");
+    const menuWrapper = pageHeader.querySelector(".menu-wrapper");
+    const level1Links = pageHeader.querySelectorAll(".level-1 > li > a");
+    const listWrapper2 = pageHeader.querySelector(".list-wrapper:nth-child(2)");
+    const listWrapper3 = pageHeader.querySelector(".list-wrapper:nth-child(3)");
+    const subMenuWrapper2 = listWrapper2.querySelector(".sub-menu-wrapper");
+    const subMenuWrapper3 = listWrapper3.querySelector(".sub-menu-wrapper");
+    const backOneLevelBtns = pageHeader.querySelectorAll(".back-one-level");
+    const isVisibleClass = "is-visible";
+    const isActiveClass = "is-active";
+    
+    toggleMenu.addEventListener("click", function () {
+      menuWrapper.classList.toggle(isVisibleClass);
+      if (!this.classList.contains(isVisibleClass)) {
+        listWrapper2.classList.remove(isVisibleClass);
+        listWrapper3.classList.remove(isVisibleClass);
+        const menuLinks = menuWrapper.querySelectorAll("a");
+        for (const menuLink of menuLinks) {
+          menuLink.classList.remove(isActiveClass);
+        }
+      }
     });
+    
+    for (const level1Link of level1Links) {
+      level1Link.addEventListener("click", function (e) {
+        const siblingList = level1Link.nextElementSibling;
+        if (siblingList) {
+          e.preventDefault();
+          this.classList.add(isActiveClass);
+          const cloneSiblingList = siblingList.cloneNode(true);
+          subMenuWrapper2.innerHTML = "";
+          subMenuWrapper2.append(cloneSiblingList);
+          listWrapper2.classList.add(isVisibleClass);
+        }
+      });
+    }
+    
+    listWrapper2.addEventListener("click", function (e) {
+      const target = e.target;
+      if (target.tagName.toLowerCase() === "a" && target.nextElementSibling) {
+        const siblingList = target.nextElementSibling;
+        e.preventDefault();
+        target.classList.add(isActiveClass);
+        const cloneSiblingList = siblingList.cloneNode(true);
+        subMenuWrapper3.innerHTML = "";
+        subMenuWrapper3.append(cloneSiblingList);
+        listWrapper3.classList.add(isVisibleClass);
+      }
+    });
+    
+    for (const backOneLevelBtn of backOneLevelBtns) {
+      backOneLevelBtn.addEventListener("click", function () {
+        const parent = this.closest(".list-wrapper");
+        parent.classList.remove(isVisibleClass);
+        parent.previousElementSibling
+          .querySelector(".is-active")
+          .classList.remove(isActiveClass);
+      });
+    }
+    
   },
 };
 
 var navigationDesktop = {
   init: function () {
+    if (window.innerWidth > 1024) {   
     const openMenu = document.querySelectorAll(".main__menu__item__holder");
     const closeMenu = document.querySelectorAll(".close__nav");
     const search = document.querySelector(".icon__search");
@@ -152,5 +201,6 @@ var navigationDesktop = {
           .classList.remove("is__active");
       }
     });
+  }
   },
 };
