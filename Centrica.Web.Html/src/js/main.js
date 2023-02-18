9874d05886fa09af24514c0c820bce5b12b550c6
manuele sapia <manulon12@outlook.com>
2023-02-18 07:10:59 +0000

    few more chnages

--- Centrica.Web.Html/src/js/main.js
+++ Centrica.Web.Html/src/js/main.js
@@ -15,7 +15,25 @@ function preCheck() {
 }
 function init() {
   navigationDesktop.init();
+  navigationMobile.init();
 }
+var navigationMobile = {
+  init: function () {
+    const mobileButton = document.querySelector("button.mobile__menu");
+    mobileButton.addEventListener("click", function () {
+      mobileButton.classList.toggle("is-open");
+      document.querySelector(".main__menu").classList.toggle("is__active");
+      // if (mobileButton.className.includes("is-open")) {
+      //   document
+      //     .querySelector(".sub__menu__main")
+      //     .classList.remove("is__active");
+      //   document
+      //     .querySelector(".main__menu__item__holder")
+      //     .classList.remove("is__active");
+      // }
+    });
+  },
+};
 
 var navigationDesktop = {
   init: function () {
@@ -31,18 +49,23 @@ var navigationDesktop = {
     // top menu items
     for (const [elemIndex, elem] of openMenu.entries()) {
       elem.addEventListener("click", function () {
-        let current = this;
-
-        for (const elem of openMenu) {
-          if (current != elem) {
-            elem.classList.remove("is__active");
-          } else if (current.classList.contains("is__active") === true) {
-            current.classList.remove("is__active");
-          } else {
-            current.classList.add("is__active");
-            for (const item of elem.querySelectorAll(".is__active")) {
+        for (const element of openMenu) {
+          if (element === elem && !element.classList.contains("is__active")) {
+            element.classList.add("is__active");
+            for (const item of element.parentElement.querySelectorAll(
+              ".sub__menu__main .is__active"
+            )) {
               item.classList.remove("is__active");
             }
+            document
+              .querySelector(".search__navigation")
+              .classList.remove("is-open");
+            element.parentElement
+              .querySelector(".close__nav")
+              .classList.remove("is__active");
+            search.classList.remove("is-open");
+          } else {
+            element.classList.remove("is__active");
           }
         }
 
@@ -100,6 +123,7 @@ var navigationDesktop = {
       });
     }
 
+    // close menu button
     for (const closeButton of closeMenu) {
       closeButton.addEventListener("click", function () {
         closeButton.classList.toggle("is__active");
@@ -108,11 +132,13 @@ var navigationDesktop = {
             .closest(".sub__menu__main")
             .classList.remove("is__active");
           closeButton
-            .closest(".main__menu__item__holder")
+            .closest(".main__menu__item")
+            .querySelector(".main__menu__item__holder")
             .classList.remove("is__active");
         }
       });
     }
+    // search button
 
     search.addEventListener("click", function () {
       search.classList.toggle("is-open");
@@ -123,7 +149,7 @@ var navigationDesktop = {
           .classList.remove("is__active");
         document
           .querySelector(".main__menu__item__holder")
-          .item.classList.remove("is__active");
+          .classList.remove("is__active");
       }
     });
   },
